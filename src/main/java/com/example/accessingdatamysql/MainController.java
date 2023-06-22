da620abb3227290474f1a5e62ceebc258c144999
rex0412 <rex041200@naver.com>
2023-06-22 15:34:30 +0900

    update and delete

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,9 +30,27 @@ public class MainController {
 		return "Saved";
 	}
 
+	@DeleteMapping(path="/user")
+	public @ResponseBody String deleteUser (@RequestParam Integer id) {
+		User n = new User();
+		n.setId(id);
+		userRepository.delete(n);
+		return "delete";
+	}
+
 	@GetMapping(path="/users")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
+	}
+
+	@PutMapping(path="/user")
+	public @ResponseBody String updateUser (@RequestParam Integer id, @RequestParam String name, @RequestParam String email) {
+		User n = new User();
+		n.setId(id);
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "updated";
 	}
 }
